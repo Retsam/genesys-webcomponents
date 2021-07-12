6ba31dc857c6b8dc42e4a4dd97f8175dd5bf7589
Katie Bobbe <katie.bobbe@genesys.com>
2021-07-12 13:38:06 -0400

    chore(gux-basic-tabs): fixed build error

--- src/components/beta/gux-basic-tabs/gux-basic-tab/gux-basic-tab-icon-title/gux-basic-tab-icon-title.tsx
+++ src/components/beta/gux-basic-tabs/gux-basic-tab/gux-basic-tab-icon-title/gux-basic-tab-icon-title.tsx
@@ -1,7 +1,7 @@
 import { Component, h } from '@stencil/core';
 
 @Component({
-  styleUrl: 'gux-basic-tab-icon-title.less',
+  //   styleUrl: 'gux-basic-tab-icon-title.less',
   tag: 'gux-basic-tab-icon-title'
 })
 export class GuxBasicTabIconTitle {
--- src/components/beta/gux-basic-tabs/gux-basic-tab/gux-basic-tab-title/gux-basic-tab-title.tsx
+++ src/components/beta/gux-basic-tabs/gux-basic-tab/gux-basic-tab-title/gux-basic-tab-title.tsx
@@ -1,7 +1,7 @@
 import { Component, h } from '@stencil/core';
 
 @Component({
-  styleUrl: 'gux-basic-tab-title.less',
+  //   styleUrl: 'gux-basic-tab-title.less',
   tag: 'gux-basic-tab-title'
 })
 export class GuxBasicTabTitle {
